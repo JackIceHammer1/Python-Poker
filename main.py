6f9a7b2c5be64e5d3ea7bda58cf69452c093142b
Jack <jackthor0608@gmail.com>
2024-07-21 23:31:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,11 @@ class PokerGame:
         self.deck = Deck()
         self.player_hand = Hand()
         self.dealer_hand = Hand()
-        self.game_over = False
         self.player_blackjack = False
         self.dealer_blackjack = False
+        self.player_bankroll = 1000
+        self.player_bet = 0
+        self.game_over = False
     
     def deal_initial_cards(self):
         for _ in range(2):
@@ -91,33 +93,68 @@ class PokerGame:
     
     def determine_winner(self):
         if self.player_blackjack and not self.dealer_blackjack:
+            self.player_bankroll += self.player_bet * 1.5
             return "Player wins with Blackjack!"
         if self.dealer_blackjack and not self.player_blackjack:
+            self.player_bankroll -= self.player_bet
             return "Dealer wins with Blackjack!"
         
         player_value = self.player_hand.calculate_value()
         dealer_value = self.dealer_hand.calculate_value()
         
         if player_value > 21:
+            self.player_bankroll -= self.player_bet
             return "Dealer wins!"
         elif dealer_value > 21 or player_value > dealer_value:
+            self.player_bankroll += self.player_bet
             return "Player wins!"
         elif player_value < dealer_value:
+            self.player_bankroll -= self.player_bet
             return "Dealer wins!"
         else:
             return "It's a tie!"
     
+    def place_bet(self):
+        while True:
+            try:
+                bet = int(input(f'You have ${self.player_bankroll}. Place your bet: '))
+                if 0 < bet <= self.player_bankroll:
+                    self.player_bet = bet
+                    break
+                else:
+                    print(f'Invalid bet amount. You have ${self.player_bankroll}.')
+            except ValueError:
+                print('Please enter a valid number.')
+    
+    def reset_game(self):
+        self.player_hand = Hand()
+        self.dealer_hand = Hand()
+        self.player_blackjack = False
+        self.dealer_blackjack = False
+        self.game_over = False
+    
     def play(self):
-        self.deal_initial_cards()
-        self.show_hands()
-        
-        if not self.player_blackjack and not self.dealer_blackjack:
-            self.player_turn()
-            if not self.game_over:
-                self.dealer_turn()
-            self.show_hands(reveal_dealer=True)
-        
-        print(self.determine_winner())
+        while True:
+            self.reset_game()
+            self.place_bet()
+            self.deal_initial_cards()
+            self.show_hands()
+            
+            if not self.player_blackjack and not self.dealer_blackjack:
+                self.player_turn()
+                if not self.game_over:
+                    self.dealer_turn()
+                self.show_hands(reveal_dealer=True)
+            
+            print(self.determine_winner())
+            
+            if self.player_bankroll <= 0:
+                print('You are out of money! Game over.')
+                break
+            
+            play_again = input('Do you want to play another round? (Y/N): ').upper()
+            if play_again != 'Y':
+                break
 
 # Example usage
 if __name__ == "__main__":
